f66870428159fbe00c1bcbd26c6e6ffe61c43afb
andrew <neon.duncan@gmail.com>
2021-08-03 15:29:56 -0500

    commit tree 8f2b13358f81afd3b585e1865c9f70236a585c8c

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
 
+class Book 
+
+  def initialize(title)
+    @title = title
+  end
+
+  attr_reader :title
+  attr_accessor :author
+  attr_accessor :genre
+  attr_accessor :page_count
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
